36bb74957e541e9975cdc07dfb7abf4efaa28896
SMJSGaming <sjoerd.t@live.nl>
2022-02-09 15:11:43 +0100

    Implemented patch setup status

--- src/controllers/PatchSetupStatus.ts
+++ src/controllers/PatchSetupStatus.ts
@@ -1,8 +1,13 @@
 import { Status } from "std-node";
+import { DB } from "..";
+import { Conflict } from "../enums/Conflict";
+import { PermLevel } from "../enums/PermLevel";
 import { RequestMethod } from "../enums/RequestMethod";
 import { Controller } from "../templates/Controller";
 import { request } from "../types/request";
 import { response } from "../types/response";
+import { Authorize } from "../utils/Authorize";
+import { QueryParser } from "../utils/QueryParser";
 
 /**
  * A setup status update API controller which toggles the setup status of the specified rent item
@@ -20,9 +25,10 @@ import { response } from "../types/response";
  * 
  * - `status`: The new status boolean
  * 
+ * 
  * **Header fields:**
  * 
- * - `authorization`: The authorization token
+ * - `authorization`: The authorization query
  */
 export class PatchSetupStatus extends Controller {
 
@@ -30,7 +36,34 @@ export class PatchSetupStatus extends Controller {
         super("/setup/:itemId", RequestMethod.PATCH);
     }
 
-    protected request(_: request, response: response): void {
-        this.respond(response, Status.NOT_IMPLEMENTED);
+    protected request(request: request, response: response): void {
+        const { userId, token } = new QueryParser(request.headers.authorization || "");
+
+        if (userId && token && Authorize.isAuthorized(userId, token, PermLevel.EMPLOYEE)) {
+            if (request.body.status) {
+                const bool = request.body.status == "1" || request.body.status.toLowerCase() == "true";
+
+                DB.connect(async (error, client, release) => {
+                    if (error) {
+                        this.respond(response, Status.INTERNAL_SERVER_ERROR);
+
+                        throw error;
+                    } else {
+                        client.query("UPDATE rent_items SET setup = $1 WHERE uuid = $2", [
+                            bool,
+                            request.params.itemId
+                        ]);
+
+                        this.respond(response, Status.OK);
+                    }
+
+                    release();
+                });
+            } else {                
+                this.respond(response, Status.CONFLICT, Conflict.INVALID_FIELDS);
+            }
+        } else {
+            this.respond(response, Status.CONFLICT, Conflict.INVALID_AUTHORIZATION);
+        }
     }
 }
