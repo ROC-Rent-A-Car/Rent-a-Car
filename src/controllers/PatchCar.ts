d09ff35e8e39653ffc78075c1c8b56d3105022b2
SMJSGaming <sjoerd.t@live.nl>
2022-02-09 15:08:26 +0100

    Implemented patch car

--- src/controllers/PatchCar.ts
+++ src/controllers/PatchCar.ts
@@ -1,8 +1,13 @@
-import { Status } from "std-node";
+import { BetterObject, Status } from "std-node";
+import { DB } from "..";
+import { Conflict } from "../enums/Conflict";
+import { PermLevel } from "../enums/PermLevel";
 import { RequestMethod } from "../enums/RequestMethod";
 import { Controller } from "../templates/Controller";
 import { request } from "../types/request";
 import { response } from "../types/response";
+import { Authorize } from "../utils/Authorize";
+import { QueryParser } from "../utils/QueryParser";
 
 /**
  * A car update API controller which updates the specified fields in the car entry
@@ -29,7 +34,7 @@ import { response } from "../types/response";
  * 
  * **Header fields:**
  * 
- * - `authorization`: The authorization token
+ * - `authorization`: The authorization query
  */
 export class PatchCar extends Controller {
 
@@ -37,7 +42,39 @@ export class PatchCar extends Controller {
         super("/car/:carId", RequestMethod.PATCH);
     }
 
-    protected request(_: request, response: response): void {
-        this.respond(response, Status.NOT_IMPLEMENTED);
+    protected request(request: request, response: response): void {
+        const { userId, token } = new QueryParser(request.headers.authorization || "");
+
+        if (userId && token && Authorize.isAuthorized(userId, token, PermLevel.MANAGER)) {
+            const entries = BetterObject.entries({
+                license: request.body.license,
+                brand: request.body.brand,
+                model: request.body.model,
+                price: request.body.price,
+                image: request.body.image,
+                description: request.body.description,
+            }).filter(([ _, value ]) => value == undefined);
+
+            if (entries.length) {
+                DB.connect(async (error, client, release) => {
+                    if (error) {
+                        this.respond(response, Status.INTERNAL_SERVER_ERROR);
+
+                        throw error;
+                    } else {
+                        client.query(`UPDATE cars SET ${entries.map(([ key ], index) => `${key} = $${index + 2}`)} WHERE uuid = $1`, [
+                            request.params.carId,
+                            ...entries.map(([ _, value ]) => value)
+                        ]);
+                    }
+
+                    release();
+                });
+            } else {
+                this.respond(response, Status.CONFLICT, Conflict.INVALID_FIELDS);
+            }
+        } else {
+            this.respond(response, Status.UNAUTHORIZED, Conflict.INVALID_AUTHORIZATION);
+        }
     }
 }
