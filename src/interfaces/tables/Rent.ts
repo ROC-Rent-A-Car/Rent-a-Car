dbe66ad48b51157b8f3947c8afea176d005ee497
SMJSGaming <sjoerd.t@live.nl>
2022-02-11 10:14:48 +0100

    Fixed a small type issue

--- src/interfaces/tables/Rent.ts
+++ src/interfaces/tables/Rent.ts
@@ -3,7 +3,7 @@ import { User } from "./User";
 
 export interface Rent extends TableBase {
     user: string,
-    userObject?: User,
+    userObject: User,
     pending: boolean,
     pending_since: Date | string
 }
