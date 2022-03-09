9009fe8516f93ab14d6c54b942975f3675b06ec1
SMJSGaming <sjoerd.t@live.nl>
2022-03-09 12:49:30 +0100

    finished the login page

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -2,3 +2,33 @@
 /// <reference path="index.d.ts" />
 /// <reference path="Cookie.js" />
 /// <reference path="APIRequest.js" />
+
+document.getElementById("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    /**
+     * @type {DynamicObject<string>}
+     */
+    // @ts-ignore TS really had a stroke here
+    const { username, password, remember } = Object.fromEntries([...new FormData(event.target)])
+
+    APIRequest.request("login", "POST", {}, constructQuery({
+        username,
+        password
+    })).then(async (response) => {
+        /**
+         * @type {APIResponse<User>}
+         */
+        const { status, message } = await response.json();
+
+        if (status == 200 && typeof message != "string") {
+            sessionStorage.setItem("account", "true");
+
+            if (remember == "on") {
+                Cookie.set("user", JSON.stringify(message), message.tokenExpiration);
+            }
+        } else {
+            throw message;
+        }
+    }).catch(console.error);
+});
