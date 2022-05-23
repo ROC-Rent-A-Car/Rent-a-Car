4d571980d18f311c30ea0fe78352b977c4727e0f
SMJSGaming <sjoerd.t@live.nl>
2022-05-23 16:31:08 +0200

    Made conflict messages dutch

--- src/enums/Conflict.ts
+++ src/enums/Conflict.ts
@@ -2,10 +2,9 @@
  * A lookup table to minimize the usage of large strings when providing error messages
  */
 export enum Conflict {
-    INVALID_FIELDS = "Some fields are invalid",
-    INVALID_AUTHORIZATION = "The provided authorization was not valid",
-    INVALID_LOGIN = "Invalid login",
-    TOKEN_ERROR = "There was a problem generating the token",
-    IN_USE_ERROR = "Either the username or email is already in use",
-    POPULATION_ERROR = "There was a problem populating the object"
+    INVALID_FIELDS = "Een aantal velden zijn ongeldig",
+    INVALID_AUTHORIZATION = "De authorisatie is ongeldig",
+    INVALID_LOGIN = "Ongeldige login",
+    TOKEN_ERROR = "Er is een fout opgetreden bij het genereren van de token",
+    IN_USE_ERROR = "Deze gebruikersnaam of email is al in gebruik"
 }
