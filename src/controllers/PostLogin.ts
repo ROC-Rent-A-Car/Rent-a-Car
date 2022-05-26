682dd5cf52eb7a7bf536f1b2ff4dd7c0fd834bc3
SMJSGaming <sjoerd.t@live.nl>
2022-05-26 11:23:38 +0200

    Added login security

--- src/controllers/PostLogin.ts
+++ src/controllers/PostLogin.ts
@@ -1,4 +1,4 @@
-import { Status } from "std-node";
+import { DynamicObject, Status } from "std-node";
 import { Conflict } from "../enums/Conflict";
 import { RequestMethod } from "../enums/RequestMethod";
 import { UserResponse } from "../interfaces/responses/UserResponse";
@@ -23,37 +23,58 @@ import { Username } from "../utils/Username";
  */
 export class PostLogin extends Controller {
 
+    private readonly attempts = <DynamicObject<number[]>>{};
+
     constructor() {
         super("/login", RequestMethod.POST);
     }
 
     protected async request(request: request, response: response): Promise<void> {
-        const username = new Username(request.body.username);
-        const password = new Password(request.body.password);
+        const minimum = new Date();
 
-        // Validate the fields
-        if (username.validate() && password.validate()) {
-            // Find a user which has the provided credentials
-            this.login(request.ip, username.transform(), password.transform()).then(async (user) => {
-                if (user) {
-                    this.respond<UserResponse>(response, Status.OK, {
-                        uuid: user.uuid,
-                        username: user.username,
-                        email: user.email,
-                        phone: user.phone,
-                        postalCode: user.postal_code,
-                        permLevel: user.perm_level,
-                        token: user.token,
-                        tokenExpiration: new Date(user.token_expiration).getTime(),
-                        houseNumber: user.house_number,
-                    });
-                } else {
-                    this.respond(response, Status.UNAUTHORIZED, Conflict.INVALID_LOGIN);
-                }
-            }).catch(() => this.respond(response, Status.BAD_REQUEST, Conflict.INVALID_FIELDS));
+        minimum.setHours(minimum.getHours() - 1);
+
+        Object.values(this.attempts).filter(
+            (attempts) => attempts[0] <= +minimum
+        ).forEach(
+            (attempts) => attempts.splice(0, attempts.filter((attempt) => attempt <= +minimum).length)
+        );
+
+        // Stop providing this service after 3 failed attempts from a specific IP
+        if (this.attempts[request.ip].length < 3) {
+            const username = new Username(request.body.username);
+            const password = new Password(request.body.password);
+
+            // Validate the fields
+            if (username.validate() && password.validate()) {
+                // Find a user which has the provided credentials
+                this.login(request.ip, username.transform(), password.transform()).then(async (user) => {
+                    if (user) {
+                        this.respond<UserResponse>(response, Status.OK, {
+                            uuid: user.uuid,
+                            username: user.username,
+                            email: user.email,
+                            phone: user.phone,
+                            postalCode: user.postal_code,
+                            permLevel: user.perm_level,
+                            token: user.token,
+                            tokenExpiration: new Date(user.token_expiration).getTime(),
+                            houseNumber: user.house_number,
+                        });
+                    } else {
+                        if (request.ip != "127.0.0.1") {
+                            this.attempts[request.ip] = [ ...(this.attempts[request.ip] ?? []), Date.now() ];
+                        }
+
+                        this.respond(response, Status.UNAUTHORIZED, Conflict.INVALID_LOGIN);
+                    }
+                }).catch(() => this.respond(response, Status.BAD_REQUEST, Conflict.INVALID_FIELDS));
+            } else {
+                // Some fields were missing or weren't formatted correctly
+                this.respond(response, Status.CONFLICT, Conflict.INVALID_FIELDS);
+            }
         } else {
-            // Some fields were missing or weren't formatted correctly
-            this.respond(response, Status.CONFLICT, Conflict.INVALID_FIELDS);
+            this.respond(response, Status.UNAUTHORIZED, Conflict.INVALID_LOGIN);
         }
     }
 }
